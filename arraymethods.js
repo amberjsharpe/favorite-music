e66c7e0da34d93279eca8243e13d8b425a7844f4
Amber Sharpe <amberjsharpe@gmail.com>
2018-02-16 15:51:52 -0600

    push bunches of changes

--- arraymethods.js
+++ arraymethods.js
@@ -5,18 +5,64 @@ var artists = ["Drake", "Ed Sheeran", "Bruno Mars", "Camilla Cabello", "Post Mal
 var albums = ["Scary Hours", "Divide", "24K Magic", "Crying in the Club", "Rockstar", "Wonderland", "Damn", "Culture II", "All Your Fault", "Hopeless Fountain Kingdom"];
 
 // Create a display area on the page for each of the sections (songs, artists & albums)
-
 var displaySongs = document.getElementById("fav-songs");
-songs.forEach(function(song) {
-    displaySongs.innerHTML += `<h4>${song}</h4>`;
-})
+function displaySongArray() {
+    songs.forEach(function(song) {
+        displaySongs.innerHTML += `<h4>${song}</h4>`;
+    });
+}
+displaySongArray(); //Call function
 
 var displayArtists = document.getElementById("fav-artists");
+function displayArtistArray() {
 artists.forEach(function(artist) {
-    displayArtists.innerHTML += `<h4>${artist}</h4>`;
-})
+        displayArtists.innerHTML += `<h4>${artist}</h4>`;
+    });
+}
+displayArtistArray(); //Call function
 
 var displayAlbums = document.getElementById("fav-albums");
-albums.forEach(function(album) {
-    displayAlbums.innerHTML += `<h4>${album}</h4>`;
-})
+function displayAlbumArray() {
+    albums.forEach(function(album) {
+        displayAlbums.innerHTML += `<h4>${album}</h4>`;
+    })
+}
+displayAlbumArray(); //Call function
+
+// Submit Button and Input fields
+var submitBtn = document.getElementById("submit-btn");
+var resetBtn = document.getElementById("reset-input");
+var songInput = document.getElementById("song-input");
+var artistInput = document.getElementById("artist-input");
+var albumInput = document.getElementById("album-input");
+
+// On Submit click, add input text to arrays
+function addNew() {
+    // Add to arrays
+    songs.unshift(songInput.value);
+    console.log(songs);
+    artists.unshift(artistInput.value);
+    console.log(artists);
+    albums.unshift(albumInput.value);
+    console.log(albums);
+
+    // Refresh div
+    displaySongs.innerHTML = "";
+    displaySongArray();
+
+    displayArtists.innerHTML = "";
+    displayArtistArray();
+
+    displayAlbums.innerHTML = "";
+    displayAlbumArray();
+
+    // Reset Input field
+    songInput.value = "";
+    artistInput.value = "";
+    albumInput.value = "";
+}
+
+submitBtn.addEventListener("click", addNew);
+//resetBtn.addEventListener("click", resetInput);
+
+
